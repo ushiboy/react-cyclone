3701cfd3b8b21d7cffe935b42853ee653da261e1
ushiboy <ushiboy.dev@gmail.com>
2019-03-18 22:50:54 +0900

    fix lint

--- src/connect.js
+++ src/connect.js
@@ -6,14 +6,18 @@ function defaultMapStateToProps(state) {
 }
 
 export function connect(mapStateToProps = defaultMapStateToProps) {
-  return View => props => {
-    return (
-      <RootContext.Consumer>
-        {({ storeState, store }) => {
-          const mapProps = mapStateToProps(storeState);
-          return <View {...props} {...mapProps} dispatch={store.dispatch} />;
-        }}
-      </RootContext.Consumer>
-    );
+  return View => {
+    const Connected = props => {
+      return (
+        <RootContext.Consumer>
+          {({ storeState, store }) => {
+            const mapProps = mapStateToProps(storeState);
+            return <View {...props} {...mapProps} dispatch={store.dispatch} />;
+          }}
+        </RootContext.Consumer>
+      );
+    };
+    Connected.displayName = 'Connected';
+    return Connected;
   };
 }
